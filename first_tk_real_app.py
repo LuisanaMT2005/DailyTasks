3171c646ee591128137da6569406e9dd1a0d28e6
LuisanaMT2005 <luisanamartineztorres25@gmail.com>
2024-02-24 16:36:49 -0400

    modify first_tk_real_app.py

--- first_tk_real_app.py
+++ first_tk_real_app.py
@@ -15,25 +15,22 @@ root = tk.Tk()
 root.title("Feet to Meters")
 
 mainframe = ttk.Frame(root, padding="3 3 12 12")
-mainframe.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
+mainframe.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S), padx=5, pady=5)
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
 feet = tk.StringVar()
 feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
-feet_entry.grid(column=2, row=1, sticky=(tk.W, tk.E))
+feet_entry.grid(column=2, row=1, sticky=(tk.W, tk.E), padx=5, pady=5)
 
 meters = tk.StringVar()
-ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(tk.W, tk.E))
+ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(tk.W, tk.E), padx=5, pady=5)
 
-ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=tk.W)
+ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=tk.W, padx=5, pady=5)
 
-ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=tk.W)
-ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=tk.E)
-ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=tk.W)
-
-for child in mainframe.winfo_children():
-    child.grid_configure(padx=5, pady=5)
+ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=tk.W, padx=5, pady=5)
+ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=tk.E, padx=5, pady=5)
+ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=tk.W, padx=5, pady=5)
 
 feet_entry.focus()
 root.bind("<Return>", calculate)
