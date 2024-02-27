ef9d2d404e614ff7f719b6896d71641267da631e
LuisanaMT2005 <luisanamartineztorres25@gmail.com>
2024-02-27 13:40:45 -0400

    try fix add_tasks() function

--- src/main.py
+++ src/main.py
@@ -15,19 +15,26 @@ tasks_structure = {
 }
 
 def add_tasks(description, priority, due_date, status):
-    with open('user_tasks.json', 'a+', encoding= 'utf-8') as tasks_file:
-        #tasks = json.loads(tasks_file.read()) !TODO: Arreglar esto, da un error de decoder.
-        #amount_of_tasks = len(tasks.keys())
-
-        task = {
-            "task_%d" % (1): {
+    with open('user_tasks.json', 'r', encoding= 'utf-8') as tasks_file:
+        try:
+            tasks = json.loads(tasks_file.read())
+            amount_of_tasks = len(tasks.keys())
+            task = {
+            "task_%d" % (amount_of_tasks + 1): {
                 "description": "%s" % (description),
                 "priority": "%s" % (priority),
                 "due_date": "%s" % (due_date),
-                "status": "%s" % (status)            
+                "status": "%s" % (status)
+                }
             }
-        }
+            new_tasks_file = tasks.update(task)
+            tasks_file_w_mode = open('user_tasks.json', 'w', encoding= 'utf-8')
+            tasks_file_w_mode.write(json.dumps(new_tasks_file, indent=2))
+            tasks_file_w_mode.close()
+        except AttributeError:
+            tasks_file_a_mode = open('user_tasks.json', 'a', encoding= 'utf-8')
+            tasks_file_a_mode.write(json.dumps(task, indent=2))
 
-        json.dumps(task, indent=2) #!TODO: Revisar si esto está bien escrito. !TODO: Ver como puedo ampliar el objeto JSON, probar cargando el archivo de tareas y luego actualizar el diccionario con el método .update.
+         #!TODO: Ver como puedo ampliar el objeto JSON, probar cargando el archivo de tareas y luego actualizar el diccionario con el método .update.
 
 add_tasks("First task", "M", "28/2/2024", "TO-DO")
