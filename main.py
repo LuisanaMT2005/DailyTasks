bc19ca3caeed2090ba8533da2281c2fc52b9f33c
LuisanaMT2005 <luisanamartineztorres25@gmail.com>
2024-02-29 00:00:31 -0400

    Change main functions to commands
    Here integration of Click package start.

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
+import click as ck
 import json
 from commands import filter_commands, modification_commands, removal_commands
+import Constants as consts
 
 
 #TODO: Leer los errores que se pueden generar usando el modulo JSON e intentar generar los errores apropósito durante el testing para ver si se generan, y manejarlos (try...except) si ocurren.
 #TODO: Integrar el modulo Click para la interfaz de uso (Integrar la clase datetime de el modulo datetime para la due_date).
 
+@ck.group
+def daily_tasks():
+    pass
 
-def initialize_tasks_file():
-    """Initialize tasks file"""
+
+@daily_tasks.command
+def create_tasks_file():
+    """Create the tasks file (it will be empty)."""
     with open('Tasks.json', 'w', encoding='utf-8') as tasks_file:
         json.dump([], tasks_file)
 
 
-def add_task(description, priority, due_date):
-    """Add a task"""
+@daily_tasks.command
+@ck.option('-d', '--description', required=True, type=ck.STRING, help="Describe your task.")
+@ck.option('-p', '--priority', type=ck.Choice(consts.PRIORITIES, case_sensitive=False), help="Choose a priority to your task.")
+@ck.option('-dd', '--due-date', type=ck.DateTime(formats=consts.DUE_DATE_FORMAT), help="Set a due date to your task.")
+@ck.option('-s', '--status', type=ck.Choice(consts.STATUS, case_sensitive=False), help="Choose a status to your task.")
+def add_task(description, priority, due_date, status):
+    """Create a new task"""
     with open('Tasks.json', 'r', encoding='utf-8') as tasks_file_read:
         tasks = json.load(tasks_file_read)
 
@@ -25,7 +37,7 @@ def add_task(description, priority, due_date):
             "description": f"{description}",
             "priority": f"{priority}",
             "due_date": f"{due_date}",
-            "status": ""
+            "status": f"{status}"
         }
     )
     
@@ -33,8 +45,9 @@ def add_task(description, priority, due_date):
         json.dump(tasks, tasks_file_write, indent=2)
 
 
+@daily_tasks.command
 def view_tasks():
-    """View all your tasks"""
+    """Shows you all your tasks"""
     with open('Tasks.json', 'r', encoding='utf-8') as tasks_file:
         tasks = json.load(tasks_file)
 
@@ -55,3 +68,6 @@ def view_tasks():
 #filter_tasks_by_priority("H")
 #filter_tasks_by_due_date("2024/2/29")
 #filter_tasks_by_status("Done")
+
+if __name__ == '__main__':
+    daily_tasks()
