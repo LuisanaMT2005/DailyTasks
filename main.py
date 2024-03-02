e74bd8b30d63da518b1452bf0ae510c1e06f61e9
LuisanaMT2005 <luisanamartineztorres25@gmail.com>
2024-03-02 13:12:19 -0400

    Get default value for due-date option

--- main.py
+++ main.py
@@ -8,6 +8,16 @@ import Constants as consts
 #TODO: Leer los errores que se pueden generar usando el modulo JSON e intentar generar los errores apropósito durante el testing para ver si se generan, y manejarlos (try...except) si ocurren.
 #TODO: Integrar el modulo Click para la interfaz de uso (Integrar la clase datetime de el modulo datetime para la due_date).
 
+def get_due_date_default_value():
+    """Get default value for due_date parameter"""
+    today_datetime_obj = datetime.today()
+    today_date_obj = today_datetime_obj.date()
+    today_date_obj_formated = today_date_obj.strftime(consts.DUE_DATE_FORMAT[0])
+    today_date_str_formated = str(today_date_obj_formated)
+
+    return today_date_str_formated
+
+# MAIN COMMANDS
 @ck.group
 def daily_tasks():
     pass
@@ -21,12 +31,29 @@ def create_tasks_file():
 
 
 @daily_tasks.command
-@ck.option('-d', '--description', required=True, type=ck.STRING, help="Describe your task.")
-@ck.option('-p', '--priority', type=ck.Choice(consts.PRIORITIES, case_sensitive=False), help="Choose a priority to your task.", default=consts.PRIORITIES[3])
-@ck.option('-dd', '--due-date', type=ck.DateTime(formats=consts.DUE_DATE_FORMAT), help="Set a due date to your task.", default=datetime.now())
-@ck.option('-s', '--status', type=ck.Choice(consts.STATUS, case_sensitive=False), help="Choose a status to your task.", default=consts.STATUS[3])
+@ck.option('-d', '--description',
+           required=True, 
+           type=ck.STRING, 
+           help="Describe your task."
+           )
+@ck.option('-p', '--priority',
+           type=ck.Choice(consts.PRIORITIES, case_sensitive=False),
+           help="Choose a priority to your task.",
+           default=consts.PRIORITIES[3]
+           )
+@ck.option('-dd', '--due-date',
+           type=ck.DateTime(formats=consts.DUE_DATE_FORMAT),
+           help="Set a due date to your task.",
+           default=get_due_date_default_value()
+           )
+@ck.option('-s', '--status',
+           type=ck.Choice(consts.STATUS, case_sensitive=False),
+           help="Choose a status to your task.",
+           default=consts.STATUS[3]
+           )
 def add_task(description, priority, due_date, status):
     """Create a new task"""
+
     with open('Tasks.json', 'r', encoding='utf-8') as tasks_file_read:
         tasks = json.load(tasks_file_read)
 
@@ -50,6 +77,8 @@ def add_task(description, priority, due_date, status):
     with open('Tasks.json', 'w', encoding='utf-8') as tasks_file_write:
         json.dump(tasks, tasks_file_write, indent=2)
 
+    return tasks[-1]
+
 
 @daily_tasks.command
 def view_tasks():
@@ -60,20 +89,6 @@ def view_tasks():
     for task in tasks:
         print(task)
 
-
-### TESTS ###
-#initialize_tasks_file()
-#add_task("First task", "m", "28/2/2024")
-#view_tasks()
-#modify_description(1, "Change task")
-#modification_commands.modify_priority(1, "H")
-#modify_due_date(1, "2024-2-29")
-#modification_commands.modify_status(1, "In progress")
-#delete_done_tasks()
-#delete_task(1)
-#filter_commands.filter_tasks_by_priority("H")
-#filter_tasks_by_due_date("2024/2/29")
-#filter_commands.filter_tasks_by_status("Done")
 
 if __name__ == '__main__':
     daily_tasks()
