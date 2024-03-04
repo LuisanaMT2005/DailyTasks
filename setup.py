e9a75ab440ac870d949b20e26bc4668918db994e
LuisanaMT2005 <luisanamartineztorres25@gmail.com>
2024-03-03 23:38:35 -0400

    Update setup.py, CONTRIBUTING, README, .gitignore files
    Here I improved the setup.py file.

--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 install_requires = [
-    "click>=8.17",
-    "coloroma>=0.4.6"
+    "click>=8.1.7",
+    "colorama>=0.4.6"
 ]
 
 setup(
-    name="daily_tasks",
+    name="DailyTasks",
     version="0.1.0",
-    description="",
-    long_description="",
-    author="",
+    description="A tasks manager for those who like work from shell.",
+    author="LuisanaMT",
+    author_email="luisanamartineztorres25@gmail.com",
+    license="Apache-2.0",
+    classifiers=["CLI"],
     install_requires=install_requires,
-    license="Apache-2.0"
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires=">=3.11",
+    entry_points={
+        'console_scripts': [
+            'dailytasks = daily_tasks.commands.main:daily_tasks',
+        ],
+    },
 )
