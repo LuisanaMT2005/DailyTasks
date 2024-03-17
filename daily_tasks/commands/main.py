be61ebf74b341aac53a656d540027bc05f62ca6e
LuisanaMTDev <luisanamartineztorres25@gmail.com>
2024-03-17 18:36:56 -0400

    [#21-Issue] Convert filter functions into commands

--- daily_tasks/commands/main.py
+++ daily_tasks/commands/main.py
@@ -2,6 +2,7 @@ import click as ck
 from daily_tasks.commands.main_commands import create_tasks_file, add_task, view_tasks
 from daily_tasks.commands.modification_commands import modify_description, modify_priority, modify_due_date, modify_status
 from daily_tasks.commands.removal_commands import delete_done_tasks, delete_task
+from daily_tasks.commands.filter_commands import filter_tasks_by_priority, filter_tasks_by_due_date, filter_tasks_by_status
 
 
 @ck.group
@@ -18,3 +19,6 @@ daily_tasks.add_command(modify_due_date)
 daily_tasks.add_command(modify_status)
 daily_tasks.add_command(delete_done_tasks)
 daily_tasks.add_command(delete_task)
+daily_tasks.add_command(filter_tasks_by_priority)
+daily_tasks.add_command(filter_tasks_by_due_date)
+daily_tasks.add_command(filter_tasks_by_status)
