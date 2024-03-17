73f320ed0b23f2e636f4a70efb5a186b057dddac
LuisanaMTDev <luisanamartineztorres25@gmail.com>
2024-03-17 18:44:57 -0400

    Update setup.py file

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ install_requires = [
 
 setup(
     name="DailyTasks",
-    version="0.4.0",
+    version="1.0.0",
     description="A tasks manager for those who like work from shell.",
     author="LuisanaMT",
     author_email="luisanamartineztorres25@gmail.com",
