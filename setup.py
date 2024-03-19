d72549cba97c70fa9aabb34fb5f94017bd8de29f
LuisanaMTDev <luisanamartineztorres25@gmail.com>
2024-03-18 21:14:07 -0400

    Update to 1.0.1 version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('README.md', 'r', encoding='utf-8') as readme_file:
 
 setup(
     name="dailytasks",
-    version="1.0.0",
+    version="1.0.1",
     description="A tasks manager for those who like work from shell.",
     long_description=long_description,
     long_description_content_type='text/markdown',
