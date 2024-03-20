8f6f0bc6442e4138330ac3269887bbb47eb4ba47
LuisanaMTDev <luisanamartineztorres25@gmail.com>
2024-03-20 18:57:49 -0400

    [#28-Issue] Simplify removal_commands.py cmds name and cmds

--- daily_tasks/commands/main.py
+++ daily_tasks/commands/main.py
@@ -1,7 +1,7 @@
 import click as ck
 from daily_tasks.commands.main_commands import add, view
 from daily_tasks.commands.modification_commands import modify
-from daily_tasks.commands.removal_commands import delete_done_tasks, delete_task
+from daily_tasks.commands.removal_commands import delete
 from daily_tasks.commands.filter_commands import filter_tasks
 
 
@@ -13,6 +13,5 @@ def daily_tasks() -> None:
 daily_tasks.add_command(add)
 daily_tasks.add_command(view)
 daily_tasks.add_command(modify)
-daily_tasks.add_command(delete_done_tasks)
-daily_tasks.add_command(delete_task)
+daily_tasks.add_command(delete)
 daily_tasks.add_command(filter_tasks, name='filter')
--- daily_tasks/commands/removal_commands.py
+++ daily_tasks/commands/removal_commands.py
@@ -4,38 +4,34 @@ from daily_tasks.commands import utilities
 
 
 @ck.command
-def delete_done_tasks():
-    """Delete all your done tasks."""
+@ck.option(
+    '-id', '--task-id',
+    type=ck.INT,
+    required=False
+)
+@ck.option(
+    '--done',
+    is_flag=True,
+    required=False
+)
+def delete(task_id, done):
+    """Delete tasks."""
     with open(utilities.tasks_file_path, 'r', encoding='utf-8') as reading_tasks_file:
         tasks = json.load(reading_tasks_file)
 
-    done_status = utilities.STATUS[2]
+    if task_id:
+        for task in tasks:
+            if task['id'] == task_id:
+                task_index = tasks.index(task)
+                tasks.pop(task_index)
 
     for task in tasks:
-        if task['status'] == done_status:
-            task_index = tasks.index(task)
-            tasks.pop(task_index)
-    
-    with open(utilities.tasks_file_path, 'w', encoding='utf-8') as writing_tasks_file:
-        json.dump(tasks, writing_tasks_file, indent=2)
+        if done:
+            done_status = utilities.STATUS[2]
 
-
-@ck.command
-@ck.option('-id', '--task-id',
-           required=True,
-           type=ck.INT)
-def delete_task(task_id):
-    """Delete a task."""
-    with open(utilities.tasks_file_path, 'r', encoding='utf-8') as reading_tasks_file:
-        tasks = json.load(reading_tasks_file)
-
-    for task in tasks:
-        if task['id'] == task_id:
-            task_index = tasks.index(task)
-            tasks.pop(task_index)
-            ck.echo(ck.style('Task deleted successfully.',
-                             bold=True))
+            if task['status'] == done_status:
+                task_index = tasks.index(task)
+                tasks.pop(task_index)
 
     with open(utilities.tasks_file_path, 'w', encoding='utf-8') as writing_tasks_file:
         json.dump(tasks, writing_tasks_file, indent=2)
-
