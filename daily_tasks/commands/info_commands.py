6972ab182befd7f73bbd600dc7ea1b11d0b851a9
LuisanaMTDev <luisanamartineztorres25@gmail.com>
2024-05-14 19:22:37 -0400

    Refactor

--- daily_tasks/commands/info_commands.py
+++ daily_tasks/commands/info_commands.py
@@ -6,11 +6,11 @@ from daily_tasks.commands.main_commands import add
 
 
 @ck.command
-@ck.option(
-    '-p', '--export-path',
-    type=ck.Path(exists=True, dir_okay=True, resolve_path=True),
-    required=True
-)
+@ck.option('-p', '--export-path',
+           type=ck.Path(exists=True,
+                        dir_okay=True,
+                        resolve_path=True),
+           required=True)
 def export_tasks(export_path):
     """Export your tasks."""
 
@@ -18,33 +18,33 @@ def export_tasks(export_path):
         tasks = json.load(tasks_file)
 
     with open(join(export_path, utilities.EXPORTED_TASKS_FILE), 'w', encoding='utf-8') as exported_tasks_file:
-        json.dump(tasks, exported_tasks_file, indent=2) 
+        json.dump(tasks, exported_tasks_file, indent=2)
+
+    # !NOTA: Agregar código para exportar las sub-tareas.
+
 
 @ck.command
-@ck.option(
-    '-a', '--add-tasks',
-    is_flag=True,
-    required=False,
-    help="To add tasks from a json file."
-)
-@ck.option(
-    '-f', '--file-name',
-    required=False,
-    help="Name of the json file where tasks you want add are."
-)
-@ck.option(
-    '-d', '--description-key',
-    required=False,
-    help="Key that contains the descriptions for your tasks."
-)
-@ck.option(
-    '-p', '--import-path',
-    type=ck.Path(exists=True, dir_okay=True, resolve_path=True),
-    required=True,
-    help=f'Directory/Folder where your {utilities.EXPORTED_TASKS_FILE} is, \n Or where your json file with tasks that you want add is (to pass this json file use -a flag).'
-)
+@ck.option('-a', '--add-tasks',
+           is_flag=True,
+           required=False,
+           help="To add tasks from a json file.")
+@ck.option('-f', '--file-name',
+           required=False,
+           help="Name of the json file where tasks you want add are.")
+@ck.option('-d', '--description-key',
+           required=False,
+           help="Key that contains the descriptions for your tasks.")
+@ck.option('-p', '--import-path',
+           type=ck.Path(exists=True, dir_okay=True,
+                        resolve_path=True),
+           required=True,
+           help=f"""Directory/Folder where your {utilities.EXPORTED_TASKS_FILE} is,
+           Or where your json file with tasks that you want add is
+           (to pass this json file use -a flag).""")
 @ck.pass_context
-def import_tasks(ctx, import_path, file_name: str | None , description_key: str | None, add_tasks=False):
+def import_tasks(ctx, import_path,
+                 file_name: str | None, description_key: str | None,
+                 add_tasks=False):
     """Import your tasks."""
 
     with open(utilities.TASKS_FILE_PATH, 'r', encoding='utf-8') as tasks_file:
@@ -59,6 +59,8 @@ def import_tasks(ctx, import_path, file_name: str | None , description_key: str 
 
         with open(utilities.TASKS_FILE_PATH, 'w', encoding='utf-8') as tasks_file:
             json.dump(existing_tasks, tasks_file, indent=2)
+
+        # !NOTA: Agregar código para importar las sub-tareas.
     else:
         with open(join(import_path, f'{file_name}.json'), 'r', encoding='utf-8') as new_tasks_file:
             new_tasks = json.load(new_tasks_file)
@@ -70,4 +72,3 @@ def import_tasks(ctx, import_path, file_name: str | None , description_key: str 
 
         for new_task_description in new_tasks_descriptions:
             ctx.invoke(add, description=new_task_description)
-            
--- daily_tasks/commands/main_commands.py
+++ daily_tasks/commands/main_commands.py
@@ -1,4 +1,3 @@
-"""Main commands of this CLI"""
 import json
 
 import click as ck
@@ -9,54 +8,47 @@ from daily_tasks.commands import utilities
 @ck.option('-d', '--description',
            required=True,
            type=ck.STRING,
-           help="Describe your task."
-           )
+           help="Describe your task.")
 @ck.option('-p', '--priority',
-           type=ck.Choice(utilities.PRIORITIES, case_sensitive=False),
+           type=ck.Choice(utilities.PRIORITIES,
+                          case_sensitive=False),
            help="Choose a priority to your task.",
-           default=utilities.PRIORITIES[3]
-           )
+           default=utilities.PRIORITIES[3])
 @ck.option('-dd', '--due-date',
            type=ck.DateTime(formats=utilities.DUE_DATE_FORMAT),
            help="Set a due date to your task.",
-           default=utilities.get_due_date_default_value()
-           )
+           default=utilities.get_due_date_default_value())
 @ck.option('-s', '--status',
-           type=ck.Choice(utilities.STATUS, case_sensitive=False),
+           type=ck.Choice(utilities.STATUS,
+                          case_sensitive=False),
            help="Choose a status to your task.",
-           default=utilities.STATUS[3]
-           )
+           default=utilities.STATUS[3])
 @ck.option('-sub', '--sub-task',
            is_flag=True,
            required=False,
-           help="Indicate that your task is a sub-task."
-           )
+           help="Indicate that your task is a subtask.")
 @ck.option('-id', '--task-id',
            type=ck.INT,
            required=False,
-           help="Pass task id it belongs to if your task is a sub-task."
-           )
+           help="If your task is a subtask, pass the task id it belongs to.")
 @ck.option('--tasks-file-path',
            hidden=True,
            required=False,
            type=ck.STRING,
            default=utilities.TASKS_FILE_PATH,
-           help="This option is ONLY FOR TESTING."
-           )
+           help="This option is ONLY FOR TESTING.")
 @ck.option('--subtasks-file-path',
            hidden=True,
            required=False,
            type=ck.STRING,
            default=utilities.SUBTASKS_FILE_PATH,
-           help="This option is ONLY FOR TESTING."
-           )
+           help="This option is ONLY FOR TESTING.")
 def add(description, priority,
         due_date, status,
         sub_task, task_id,
         tasks_file_path=utilities.TASKS_FILE_PATH,
-        subtasks_file_path=utilities.SUBTASKS_FILE_PATH
-        ) -> None: # pylint: disable=unused-argument
-    """Create a new task."""
+        subtasks_file_path=utilities.SUBTASKS_FILE_PATH) -> None:
+    """Create a new task or subtask."""
     if sub_task is False:
         try:
             with open(tasks_file_path, 'r', encoding='utf-8') as tasks_file_read:
@@ -80,7 +72,7 @@ def add(description, priority,
                 "priority": f"{priority_upper}",
                 "due_date": f"{due_date_formatted}",
                 "status": f"{status_capitalize}",
-                "sub_tasks": []
+                "subtasks": []
             }
         )
 
@@ -89,17 +81,16 @@ def add(description, priority,
     else:
         try:
             with open(subtasks_file_path, 'r', encoding='utf-8') as tasks_file_read:
-                sub_tasks = json.load(tasks_file_read)
+                subtasks = json.load(tasks_file_read)
         except json.decoder.JSONDecodeError:
-            sub_tasks = []
+            subtasks = []
 
-        if sub_tasks == []:
+        if subtasks == []:
             new_id = 1
         else:
-            last_sub_task = sub_tasks[-1]
+            last_sub_task = subtasks[-1]
             last_id = last_sub_task['id']
             new_id = last_id + 1
-        
 
         with open(tasks_file_path, 'r', encoding='utf-8') as tasks_file_read:
             tasks: list = json.load(tasks_file_read)
@@ -108,19 +99,19 @@ def add(description, priority,
             if task['id'] == task_id:
                 extracted_task: dict = task
                 break
-            raise Exception("A task with the passed id doesn't exist.") # pylint: disable=broad-exception-raised
+            raise ValueError("A task with the passed id doesn't exist.")
 
         try:
             sub_tasks_of_extracted_task: list = extracted_task['sub_tasks']
             sub_tasks_of_extracted_task.append(new_id)
         except KeyError:
             extracted_task.update(
-                {"sub_tasks": [new_id]}
+                {"subtasks": [new_id]}
             )
 
         priority_upper, due_date_formatted, status_capitalize = utilities.format_priority_status_and_due_date(priority, status, due_date)
 
-        sub_tasks.append(
+        subtasks.append(
             {
                 "id": new_id,
                 "description": f"{description}",
@@ -132,10 +123,11 @@ def add(description, priority,
         )
 
         with open(subtasks_file_path, 'w', encoding='utf-8') as sub_tasks_file_write:
-            json.dump(sub_tasks, sub_tasks_file_write, indent=2)
+            json.dump(subtasks, sub_tasks_file_write, indent=2)
 
         with open(tasks_file_path, 'w', encoding='utf-8') as tasks_file_write:
             json.dump(tasks, tasks_file_write, indent=2)
+
 
 @ck.command
 @ck.option('--tasks-file-path',
@@ -145,7 +137,7 @@ def add(description, priority,
            default=utilities.TASKS_FILE_PATH,
            help="This option is ONLY FOR TESTING."
            )
-def view(tasks_file_path=utilities.TASKS_FILE_PATH) -> None: # pylint: disable=unused-argument
+def view(tasks_file_path=utilities.TASKS_FILE_PATH) -> None:
     """View all your tasks."""
     with open(tasks_file_path, 'r', encoding='utf-8') as tasks_file:
         tasks = json.load(tasks_file)
@@ -158,5 +150,7 @@ def view(tasks_file_path=utilities.TASKS_FILE_PATH) -> None: # pylint: disable=u
         due_date = task['due_date']
         status = task['status']
 
-        utilities.stylized_tasks_printing(task_id, description, priority, due_date, status)
-        #!NOTA: Crear aquí el for de las sub-tareas.
+        utilities.stylized_tasks_printing(task_id, description,
+                                          priority, due_date,
+                                          status)
+        # !NOTA: Crear aquí el for de las sub-tareas.
