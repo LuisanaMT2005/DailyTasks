a2db0b0467b417e194103909603f2d9fd9d7eeba
LuisanaMTDev <luisanamartineztorres25@gmail.com>
2024-05-14 17:57:07 -0400

    Add code to create 'subtasks.json' file

--- daily_tasks/commands/main.py
+++ daily_tasks/commands/main.py
@@ -7,7 +7,7 @@ from daily_tasks.commands.main_commands import add, view
 from daily_tasks.commands.modification_commands import modify
 from daily_tasks.commands.removal_commands import delete
 from daily_tasks.commands.filter_commands import filter_tasks
-from daily_tasks.commands.utilities import DATA_FILES_PATH, TASKS_FILE_PATH
+from daily_tasks.commands.utilities import DATA_FILES_PATH, TASKS_FILE_PATH, SUBTASKS_FILE_PATH
 
 
 
@@ -19,10 +19,13 @@ from daily_tasks.commands.utilities import DATA_FILES_PATH, TASKS_FILE_PATH
 def daily_tasks() -> None:
     """A tasks manager for those who like work from shell."""
     makedirs(DATA_FILES_PATH, exist_ok=True)
-    if lexists(join(DATA_FILES_PATH, TASKS_FILE_PATH)):
+    if lexists(join(DATA_FILES_PATH, TASKS_FILE_PATH)) and lexists(join(DATA_FILES_PATH, SUBTASKS_FILE_PATH)):
         pass
     else:
         with open(TASKS_FILE_PATH, 'w', encoding='utf-8') as tasks_file:
+            pass
+
+        with open(SUBTASKS_FILE_PATH, 'w', encoding='utf-8') as subtasks_file:
             pass
 
 
